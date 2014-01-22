232dc6952852f98d9b5492d2c85e7d932c0869c5
ZacHack <bigzac182@yahoo.com>
2014-01-21 16:05:00 -0800

    Scratched the old version because it didnt work
    Got rid of the old version because it didnt work and made this new version that semi works

--- CrazyMonkey.php
+++ CrazyMonkey.php
@@ -1,55 +1,27 @@
 <?php
-/*
-	CrazyMonkey is an IRCBot made by ZacHack
+/* 
+This is an IRC Bot made by ZacHack
+DO NOT Sell this product
+DO NOT claim this product is yours
 */
 set_time_limit(0);
 ini_set('display_errors', 'on');
-$config = array(
-	'server' => 'irc.freenode.net',
-	'port' => '6667',
-	'nick' => 'CrazyMonkey',
-	'name' => 'Rob',
-	'pass' => 'cmhq',
-	);
-class CrazyMonkey{
-	var $socket;
-	var $ex = array();
-	function __construct($config){
-		$this->socket = fsockopen($config['server'], $config['port']);
-		$this->login($config);
-		$this->main();
-		$this->send_data('JOIN', '#chat');
-	}
-	function login($config){
-		$this->send_data('USER', $config['nick'].' google.com'.$config['nick'].' :'.$config['name']);
-		$this->send_data('NICK', $config['nick']);
-	}
-	function main(){
-		$data = fgets($this->socket, 128);
-		echo nl2br($data);
+$socket = fsockopen('irc.rizon.net', 6668);
+fputs($socket, "USER CrazyMonkey localhost CrazyMonkey :Billy");
+fputs($socket, "NICK CrazyMonkey");
+fputs($socket, "JOIN :#ZacHackbotesting");
+fputs($socket, "PASS cmhq");
+while(1){
+	while($data = fgets($socket, 128)){
+		echo $data;
 		flush();
-		$this->ex = explode(' ', $data);
-		if($this->ex[0] == 'PING'){
-			$this->send_data('PONG', $this->ex[1]);
+		$ex = explode(' ', $data);
+		if($ex[0] == "PING"){
+			fputs($socket, "PONG ".$ex[1]."");
 		}
-		$command = str_replace(array(chr(10), chr(13)), '', $this->ex[3]);
-		switch($command){
-			case ':!join':
-				$this->join_channel($this->ex[4]);
-				break;
-			case ':!quit':
-				$this->send_data('QUIT', 'Disconnected');
-				break;
-			case ':!op':
-				$this->op_user();
-				break;
-			case ':!deop':
-				$this->op_user('','',false);
-				break;
-			case ':!speak':
-				fputs($socket, "PRIVMSG It works!\n");
-				break;
+		$cmd = str_replace(array(chr(10), chr(13)), '', $ex[3]);
+		if($cmd == ":!sayit"){
+			fputs($socket, "PRIVMSG ".$ex[2]." it works");
 		}
-		$this->main;
 	}
 }
