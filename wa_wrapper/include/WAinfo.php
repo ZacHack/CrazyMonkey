e03539d68c340dc7cb5446499f164198877f04e7
ZacHack <bigzac182@yahoo.com>
2014-05-30 10:49:22 -0700

    Create WAinfo.php

---
+++ wa_wrapper/include/WAinfo.php
@@ -0,0 +1,16 @@
+<?php
+
+/** 
+ *  The Wolfram Alpha Info Object
+ *  @package WolframAlpha
+ */
+class WAInfo {
+  // define the sections of a response
+  public $text = '';
+ 
+  // Constructor
+  public function WAInfo () {
+  }
+
+}
+?>
